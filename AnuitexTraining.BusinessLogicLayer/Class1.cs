4f19cea4390a04bfc385ea3a86a1cd2403afbe2e
vladislav.v.goncharuk <vladislav.v.goncharuk@gmail.com>
2020-08-13 11:46:31 +0300

    git init

---
+++ AnuitexTraining.BusinessLogicLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AnuitexTraining.BusinessLogicLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ AnuitexTraining.DataAccessLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AnuitexTraining.DataAccessLayer
+{
+    public class Class1
+    {
+    }
+}
